a56af701683f762092849b1007af11b5496e188f
ikrotzky <ikr@croworc.com>
2019-06-04 07:50:21 +0200

    [Step.08-Ex] refactor: Add data binding to SleepNightAdapter

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -17,10 +17,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -28,6 +25,7 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 class SleepNightAdapter : ListAdapter<SleepNight,
         SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
@@ -42,20 +40,16 @@ class SleepNightAdapter : ListAdapter<SleepNight,
         return ViewHolder.from(parent)
     }
 
-    // TODO (04) Replace the ViewHolder constructor itemView parameter with a ListItemSleepNightBinding.
-    class ViewHolder private constructor (itemView: View) : RecyclerView.ViewHolder(itemView){
-
-        // TODO (05) Replace findViewById calls with inline references to binding object fields.
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-        val quality: TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    // COMPLETED (04) Replace the ViewHolder constructor itemView parameter with a ListItemSleepNightBinding.
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding)
+        : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: SleepNight) {
             val res = itemView.context.resources
 
-            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            quality.text = convertNumericQualityToString(item.sleepQuality, res)
-            qualityImage.setImageResource(when (item.sleepQuality) {
+            binding.sleepLength.text   = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
+            binding.qualityImage.setImageResource(when (item.sleepQuality) {
                 0 -> R.drawable.ic_sleep_0
                 1 -> R.drawable.ic_sleep_1
                 2 -> R.drawable.ic_sleep_2
@@ -64,27 +58,35 @@ class SleepNightAdapter : ListAdapter<SleepNight,
                 5 -> R.drawable.ic_sleep_5
                 else -> R.drawable.ic_sleep_active
             })
-        }
+        } // close function bind()
 
         companion object {
+            /**
+             * `Static` function: provides a new ViewHolder.
+             *  Will be called from the adapter's callback onCreateViewHolder(), whenever a new
+             *  ViewHolder is needed.
+             *  @param parent The root view group into which the ViewHolder's layout will be inserted.
+             *          Here, it's just the RecyclerView itself.
+             */
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
 
-                // TODO (03) Inflate layout using binding object assigned to ListItemSleepNightBinding.
-                val view = layoutInflater
-                        .inflate(R.layout.list_item_sleep_night, parent, false)
+                // COMPLETED (03) Inflate layout using binding object assigned to ListItemSleepNightBinding.
+                val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            } // close function from()
 
-                return ViewHolder(view)
-            }
-        }
-    }
-}
+        } // close ViewHolder's companion object
+
+    } // close inner class ViewHolder
+
+} // close class SleepNightAdapter
 
 /**
  * Callback for calculating the diff between two non-null items in a list.
  *
- * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
- * list that's been passed to `submitList`.
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
+ * list that's been passed to `submitList()`.
  */
 class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
     override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
@@ -94,4 +96,4 @@ class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
     override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem == newItem
     }
-}
+} // close class SleepNightDiffCallback
