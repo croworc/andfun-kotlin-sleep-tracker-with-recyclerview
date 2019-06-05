2f7d286d923888c739386a814d6cc3173a8b856b
ikrotzky <ikr@croworc.com>
2019-06-05 11:11:38 +0200

    [Step.11-Ex] fix: Typo in function comment: `minimum`

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -64,7 +64,7 @@ class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<Sle
 /**
  * Callback for calculating the diff between two non-null items in a list.
  *
- * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
  * list that's been passed to `submitList`.
  */
 class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
