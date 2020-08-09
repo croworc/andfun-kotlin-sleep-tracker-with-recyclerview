6d116c647b0439b562018e234c2e5b6f0697617b
ikrotzky <ikr@croworc.com>
2020-08-09 20:10:09 +0200

    [Step.11-So] doc: Fix type in comment

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -60,7 +60,7 @@ class SleepNightAdapter(private val clickListener: SleepNightListener) : ListAda
 /**
  * Callback for calculating the diff between two non-null items in a list.
  *
- * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
  * list that's been passed to `submitList`.
  */
 class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
