4698c7aabfaa86941134defeb37806c8a1a9c011
ikrotzky <ikr@croworc.com>
2020-08-11 16:11:56 +0200

    [Step.14-So] refactor: Make clickListener property private
    Also: Make ITEM_VIEW_TYPE_... constants private,
    remove unnecessary curly braces in string template,
    fix typo `minumum` to `minimum`

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -30,10 +30,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-private val ITEM_VIEW_TYPE_HEADER = 0
-private val ITEM_VIEW_TYPE_ITEM = 1
+private const val ITEM_VIEW_TYPE_HEADER = 0
+private const val ITEM_VIEW_TYPE_ITEM   = 1
 
-class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<DataItem,
+class SleepNightAdapter(private val clickListener: SleepNightListener) : ListAdapter<DataItem,
         RecyclerView.ViewHolder>(SleepNightDiffCallback()) {
 
     private val adapterScope = CoroutineScope(Dispatchers.Default)
@@ -63,7 +63,7 @@ class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<Dat
         return when (viewType) {
             ITEM_VIEW_TYPE_HEADER -> TextViewHolder.from(parent)
             ITEM_VIEW_TYPE_ITEM -> ViewHolder.from(parent)
-            else -> throw ClassCastException("Unknown viewType ${viewType}")
+            else -> throw ClassCastException("Unknown viewType $viewType")
         }
     }
 
@@ -107,7 +107,7 @@ class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<Dat
 /**
  * Callback for calculating the diff between two non-null items in a list.
  *
- * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
  * list that's been passed to `submitList`.
  */
 class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
